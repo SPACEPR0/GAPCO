3156f050cbbf1f8196682b74b923b37c27bebfac
raulmyocu <rauloctaviomurcia@gmail.com>
2019-10-24 19:21:54 -0700

    login component added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   
-  root 'welcome#index'
+  devise_for :users#, :skip => [:registrations]
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
+  
+  authenticated :user do
+    # Routes for authenticated users
+    root to: 'welcome#index', as: :authenticated_root
+  end
+
+  root to: redirect('login')
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
