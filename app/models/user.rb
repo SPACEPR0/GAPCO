534a1eb9e8877102133ca1ed2e02bc28db8ad2d7
AndyHdez <sayayindelatierra@gmail.com>
2019-10-27 14:22:59 -0700

    added firstname and lastname validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :areas
+
+  validates_presence_of :firstname, :lastname
 end
