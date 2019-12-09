a430867f4ced5178a770996ef6dd8627bead90aa
AndyHdez <sayayindelatierra@gmail.com>
2019-12-09 12:11:08 -0700

    bugs fixed

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -42,7 +42,7 @@ class AreasController < ApplicationController
       redirect_to root_path, notice: "No tienes permisos para realizar esta acción"
       return
     end
-
+    areas = Area.all
     areas.order(number: :asc)
     # Create notifications
     Notification.create(recipient: @area.user, actor:current_user, action: " creó el área " + @area.name.to_s, notifiable: @area)
